db0c1ecabbcd72f6882adfb092e0871ac4baca41
Alfonso Tort <alfonso.tort@anahuac.mx>
2023-06-30 09:24:14 -0600

    inicio del curso

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+const msg = "Hola mundo";
+console.log(msg);
+const numeros = 1234;
+console.log(numeros);
+const persona = {
+    name: "persona",
+    animal: 1234
+};
+console.log(persona.animal + 1);
---
+++ app.ts
@@ -0,0 +1,13 @@
+const msg: string = "Hola mundo"
+console.log(msg)
+
+const numeros:number = 1234
+console.log(numeros)
+
+const persona = {
+    name: "persona",
+    animal: 1234
+}
+
+
+console.log(persona.animal + 1)
